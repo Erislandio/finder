75dbe62078231752f981ebe8d40d5b77603fe8bb
erislandio.soares <erislandiosoares21@gmail.com>
2020-02-01 15:11:22 -0300

    adição das rotas provider

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 const userController = require("../app/controllers/userController");
+const providerController = require("../app/controllers/providerController");
 const authController = require("../app/controllers/authController");
 const authMiddleware = require("../app/middlewares/auth");
 
@@ -16,6 +17,12 @@ router.post("/user", userController.store);
 router.get("/user", userController.index);
 router.delete("/user", authMiddleware, userController.delete);
 router.patch("/user", authMiddleware, userController.update);
+
+// Provider
+router.post("/provider", providerController.store);
+router.get("/provider", providerController.index);
+router.delete("/provider", authMiddleware, providerController.delete);
+router.patch("/provider", authMiddleware, providerController.update);
 
 // * Authentication
 router.post("/login", authController.login);
