72713431a02b86074169b7d2d46e55d1e3342b5c
erislandio.soares <erislandiosoares21@gmail.com>
2020-02-01 13:58:05 -0300

    ajustes heroku app

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = mongoose.connect(
-  process.env.MONGOURL ||
+  process.env.MONGODB_URI ||
     "mongodb+srv://erislandio:Er1sl@ndio@cluster0-ecwcd.mongodb.net/test?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
