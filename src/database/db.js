66c0712c269b6a9760c6db1ad532a08b32dbecdf
erislandio.soares <erislandiosoares21@gmail.com>
2020-02-01 13:48:23 -0300

    ajustes folder

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 module.exports = mongoose.connect(
-  "mongodb+srv://erislandio:Er1sl@ndio@cluster0-ecwcd.mongodb.net/test?retryWrites=true&w=majority",
+  process.env.MONGOURL ||
+    "mongodb+srv://erislandio:Er1sl@ndio@cluster0-ecwcd.mongodb.net/test?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
