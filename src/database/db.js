2662fb7c6213580c25c5940a07a747fee6910d13
erislandio.soares <erislandiosoares21@gmail.com>
2020-02-01 14:07:42 -0300

    ajuste scrips

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://erislandio:Er1sl@ndio@cluster0-ecwcd.mongodb.net/test?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+module.exports = mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
